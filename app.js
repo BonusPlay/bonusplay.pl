0103f482bea1e244e7e4cc44b8cf3ea52f09828f
BonusPlay <bonusplay@taw.net>
2017-04-27 23:05:17 +0200

    Minor update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(rateLimiter({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
